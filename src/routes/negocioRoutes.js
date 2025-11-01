5c1db2b499daf231ee9e7a94b119d0e74dfa5a25
Alejandraqt <alejandradurannq@gmail.com>
2025-10-31 22:09:17 -0500

    Actualización negocioRoutes, CRUD completo

--- src/routes/negocioRoutes.js
+++ src/routes/negocioRoutes.js
@@ -11,4 +11,36 @@ router.post("/negocio", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+//Consultar todos los negocios
+router.get("/negocio", (req, res) => {
+    negocioSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Modificar el nombre de un negocio por su id
+router.put("/negocio/:id", (req, res) => {
+    const { id } = req.params;
+    const { razonSocial, descripcion, telefono, correo } = req.body;
+    negocioSchema
+        .updateOne({ _id: id }, {
+            $set: { razonSocial, descripcion, telefono, correo }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+//Eliminar un negocio con su id
+router.delete("/negocio/:id", (req, res) => {
+    const { id } = req.params;
+    negocioSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
 module.exports = router;
