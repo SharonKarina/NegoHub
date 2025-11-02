4da332d7823b74fea95aae3db5f8e044d935b3f1
Sharon Cruz <sharina.1401@gmail.com>
2025-11-02 00:33:06 -0500

    Completo Usuario
    Se agrego la información y CRUD usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,1 +1,23 @@
 const mongoose = require("mongoose"); 
+
+const usuarioSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: ,
+    },
+    correo: {
+        type: String,
+        required: ,
+    },
+    contraseña: {
+        type: String,
+        required: ,
+    },
+    rol: {
+        type: ,
+        required: ,
+        unique: ,
+    }
+});
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
--- src/routes/usuarioRoures.js
+++ src/routes/usuarioRoures.js
@@ -1,3 +1,42 @@
 const express = require("express");
 const router = express.Router(); 
-const negocioSchema = require("../models/usuarioModel");
+const usuarioSchema = require("../models/usuarioModel");
+
+router.post("/usuario", (req, res) => {
+    const usuario = usuarioSchema(req.body);
+    usuario
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/usuario", (req, res) => {
+    usuarioSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.put("/usuario/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre, correo, rol } = req.body;
+    usuarioSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre, correo, rol }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.delete("/usuario/:id", (req, res) => {
+    const { id } = req.params;
+    usuarioSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
+module.exports = router;
