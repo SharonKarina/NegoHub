f27238697a43f286f98f0f62dd4166c94bc9f298
Sharon Cruz <sharina.1401@gmail.com>
2025-11-02 08:11:52 -0500

    Comentarios módulo categoría
    Se comenta categoriaModel y categoriaRoutes para mejorar el entendimiento.

--- src/models/categoriaModel.js
+++ src/models/categoriaModel.js
@@ -1,10 +1,22 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose"); //importa librebía Mongoose para gestionar la base de datos MongoDB
 
+//Esquema que define la estructura de doucumentos para la tabla 'categoria'
 const categoriaSchema = mongoose.Schema({
     nombre: {
-        type: String,
-        required: true,
+        type: String, // Tipo de dato
+        required: true, //Este campo es obligatorio
     }
 });
 
+//Exporta el modelo para poder usarlo en rutas
 module.exports = mongoose.model("categoria", categoriaSchema);
+
+
+/*
+formato para creacion en Postman 
+
+{
+    "nombre": """
+}
+
+*/
--- src/routes/categoriaRoutes.js
+++ src/routes/categoriaRoutes.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router(); //Manejador de rutas de express
-const negocioSchema = require("../models/categoriaModel");
 
+const categoriaSchema = require("../models/categoriaModel"); //Importa el modelo de Mongoose para la coleccion 'categoria'
+
+
+//Creación de una nueva categoría
 router.post("/categoria", (req, res) => {
-    const categoria = categoriaSchema(req.body);
+    const categoria = new categoriaSchema(req.body);
     categoria
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
 
+
+//Consulta de todas las categoorías
 router.get("/categoria", (req, res) => {
     categoriaSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
 
+
+//Actualización de una categoría por ID
 router.put("/categoria/:id", (req, res) => {
     const { id } = req.params;
     const { nombre } = req.body;
@@ -27,6 +34,8 @@ router.put("/categoria/:id", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+
+//Eliminar una categoría por ID
 router.delete("/categoria/:id", (req, res) => {
     const { id } = req.params;
     categoriaSchema
