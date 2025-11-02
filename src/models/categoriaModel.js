f2650475389315630d14bde9b9c7c653dea0cdf9
Sharon Cruz <sharina.1401@gmail.com>
2025-11-02 01:04:03 -0500

    Completo categoría
    Se agrego la información y CRUD categoría

--- src/models/categoriaModel.js
+++ src/models/categoriaModel.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose"); 
+
+const categoriaSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: ,
+    }
+});
+
+module.exports = mongoose.model("categoria", categoriaSchema);
--- src/routes/categoriaRoutes.js
+++ src/routes/categoriaRoutes.js
@@ -1,3 +1,42 @@
 const express = require("express");
 const router = express.Router(); //Manejador de rutas de express
 const negocioSchema = require("../models/categoriaModel");
+
+router.post("/categoria", (req, res) => {
+    const categoria = categoriaSchema(req.body);
+    categoria
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/categoria", (req, res) => {
+    categoriaSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.put("/categoria/:id", (req, res) => {
+    const { id } = req.params;
+    const { nombre } = req.body;
+    categoriaSchema
+        .updateOne({ _id: id }, {
+            $set: { nombre }
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.delete("/categoria/:id", (req, res) => {
+    const { id } = req.params;
+    categoriaSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
+module.exports = router;
